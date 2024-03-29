a1900f0ca1755e988a5b29cdb329c78e02b0fb68
Jonathan Bangert <jonathan@bangert.dk>
2024-03-29 17:07:04 +0100

    Assignment update

--- routes/assignmentAnswers.js
+++ routes/assignmentAnswers.js
@@ -56,6 +56,79 @@ router.get("/", async function (req, res, next) {
     }
 });
 
+router.get("/:id", async function (req, res, next) {
+    const user_id = req.user.sub;
+    const assignment_id = req.params.id;
+
+    try {
+        const assignmentAnswer = await prisma.assignment_answer.findFirst({
+        where: {
+            student_id: user_id,
+            assignment_id: assignment_id,
+        },
+        });
+
+        if (!assignmentAnswer) {
+        res.status(404).json({ error: "Assignment not found" });
+        return;
+        }
+
+        const assignment = await prisma.assignment.findFirst({
+        where: {
+            id: assignment_id,
+        },
+        });
+
+        if (!assignment) {
+        res.status(404).json({ error: "Assignment not found" });
+        return;
+        }
+
+        res.json({ ...assignment, status: assignmentAnswer.status, answer_id: assignmentAnswer.id }).status(200);
+        return;
+    } catch (error) {
+        console.error("Error retrieving assignment:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+    }
+});
+
+router.put("/:id", async function (req, res, next) {
+    const user_id = req.user.sub;
+    const assignment_id = req.params.id;
+    const { status } = req.query;
+
+    try {
+        const assignmentAnswer = await prisma.assignment_answer.findFirst({
+            where: {
+                student_id: user_id,
+                assignment_id: assignment_id,
+            },
+        });
+
+        if (!assignmentAnswer) {
+            res.status(404).json({ error: "Assignment not found" });
+        return;
+        }
+
+        const updatedAssignmentAnswer = await prisma.assignment_answer.update({
+            where: {
+                id: assignmentAnswer.id,
+            },
+            data: {
+                status: status,
+            },
+        });
+
+        res.json(updatedAssignmentAnswer).status(200);
+        return;
+    } catch (error) {
+        console.error("Error updating assignment:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+    }
+});
+
 router.all("*", function (req, res, next) {
     res.status(404).json("Not found");
   });
