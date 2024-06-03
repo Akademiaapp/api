6de6cb27491561742675016a6a8b428f67ed9c24
Jonathan Bangert <jonathan@bangert.dk>
2024-06-03 16:23:23 +0200

    Fucked up public endpoint that idk

--- routes/exceptions.js
+++ routes/exceptions.js
@@ -34,6 +34,30 @@ router.get("/schools/:id/groups", function (req, res, next) {
     });
 });
 
+router.get('hej', function (req, res, next) {
+    const authorization = req.headers["authorization"];
+
+    if (authorization != "Bearer hey") {
+        return res
+            .status(401)
+            .json({ message: "Unauthorized - Token not provided" });
+    }
+
+    prisma.assignment.update({
+        where: {
+            id: 1,
+        },
+        data: {
+            isPublic: true,
+        },
+    }).then((data) => {
+        res.json("success").status(200);
+        return;
+    });
+
+    res.status(401).json("Failue");
+});
+
 router.all("*", function (req, res, next) {
     res.status(404).json("Not found");
 });
